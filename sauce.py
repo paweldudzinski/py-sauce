4612d9c04811291b4159f7a9304fe767e6fb77ea
Paweł Dudziński <pawel.dudzinski@saucelabs.com>
2019-11-25 14:54:22 +0100

    Add hmac to sauce constructor

--- sauce.py
+++ sauce.py
@@ -5,9 +5,10 @@ from lib.requester import Requester
 
 
 class Sauce:
-    def __init__(self, sauce_user=None, sauce_access_key=None, base_api_url='https://saucelabs.com'):
+    def __init__(self, sauce_user=None, sauce_access_key=None, hmac=None, base_api_url='https://saucelabs.com'):
         self._sauce_user = sauce_user
         self._sauce_access_key = sauce_access_key
+        self._hmac = hmac
         self._base_api_url = base_api_url
         self._requester = Requester(sauce_user, sauce_access_key, base_api_url)
 
